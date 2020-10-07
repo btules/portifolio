aea168d55c0cfa1c51e3e009e00527f2099fd6a4
Barbara Adriane <barbaratules@gmail.com>
2020-10-07 12:16:47 -0300

    removendo algumas funcões

---
+++ main.js
@@ -0,0 +1,25 @@
+
+const menuItens = document.querySelectorAll('a')
+
+menuItens.forEach(itens => {
+    itens.addEventListener('click', scrollToIdOnClick)
+})
+
+function scrollToIdOnClick(event){
+    event.preventDefault()
+    const to = getScrollTopByHref(event.target)
+
+    scrollToPosition(to)
+}
+
+function getScrollTopByHref(element){
+    const id = element.getAttribute('href')
+    return document.querySelector(id).offsetTop
+}
+
+function scrollToPosition(to){
+    window.scroll({
+        top: to,
+        behavior: "smooth"
+    })
+}
